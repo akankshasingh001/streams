a32eb50fd74dd71aced8785214bdb1cddeae2927
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-05 17:07:47 -0400

    Component Scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
