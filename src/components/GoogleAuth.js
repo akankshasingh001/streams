b216d1240eb86933d25f29ca168b2363b283c7eb
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-06 12:07:55 -0400

    added logic for signin & signOut to google auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '631398426754-ptfcts19t0kqj76gkhgru3vnhm4f0a6u.apps.googleusercontent.com',
-        scope: 'email'
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '631398426754-ptfcts19t0kqj76gkhgru3vnhm4f0a6u.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Don't know if signed in or not</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>Signed In</div>;
+    } else {
+      return <div>Sign Out</div>;
+    }
+  }
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
